a97b63cd462b75d10ba486b63aa9e889e578e763
龙州客 <liujiantong@gmail.com>
2016-07-31 11:22:08 +0800

    requirements changed

--- beehive.py
+++ beehive.py
@@ -1,34 +1,15 @@
 #!/usr/bin/env python
 # encoding: utf8
 
-import argparse
 import random
 import math
 import logging
 
 from functools import reduce
-from itertools import chain
-
 import numpy as np
 from scipy import stats
 
-default_fig_name = 'beehive-simulation.png'
-defalut_small_pool_ratio = 0.25
-
-N_Days = 365
-
-claim_freq_lambda = 10
-
-default_honeycomb_size_in_hive = 10000
-default_bee_size_in_honeycomb = 5
-
-# 投保分布参数
-premium_mu = 1500
-premium_sigma = 400
-
-# 索赔金额分布参数
-charge_mu = 300
-charge_sigma = 100
+import conf
 
 
 class BankruptException(Exception):
@@ -37,27 +18,50 @@ class BankruptException(Exception):
 
 class Beehive:
 
-    def __init__(self, hive_id=0):
+    def __init__(self, hive_id, reserve_fund, calc_max_premium):
         self.id = hive_id
-        self.balance = 0
         self.all_honeycombs = []
+        self.reserve_fund = reserve_fund
+        self.calc_max_premium = calc_max_premium
 
-    def renew(self, premium):
-        self.balance += premium
+    def charge(self, bee0, fee):
+        """
+        此时客户小池和大池的金额已耗尽, 需要动用大池里的金额, 按余额比例分配且不超过最高限额
+        :param fee: 需要赔付的金额
+        :return:
+        """
+        sum = self.pool_balance()
+        sum1 = 0
+        for bee in self.bees_iter():
+            fee_bee = fee * (bee.pool_balance / sum)
+            max_fee = self.calc_max_premium(bee.pool_balance)
+            fee_sub = min(fee_bee, max_fee)
+            bee.pool_balance -= fee_sub
+            sum1 += fee_sub
 
-    def charge(self, fee):
-        remaining = self.balance - fee
-        self.balance = remaining
-        return remaining
+        if sum1 < fee:
+            sub_fee = fee - sum1
+            self.__charge_reserve_fund(sub_fee)
 
     def bees_iter(self):
-        return chain.from_iterable(self.all_honeycombs)
+        for comb in self.all_honeycombs:
+            for bee in comb.bees:
+                yield bee
 
     def bees(self):
         return [bee for comb in self.all_honeycombs for bee in comb.bees]
 
+    def pool_balance(self):
+        return reduce((lambda x, y: x + y), [b.pool_balance for b in self.bees_iter()])
+
+    def __charge_reserve_fund(self, fee):
+        remaining = self.reserve_fund - fee
+        if remaining <= 0:
+            raise BankruptException
+        self.reserve_fund -= remaining
+
     def __str__(self):
-        str_val = "Beehive simulation | balance:%d\n\n" % self.balance
+        str_val = "Beehive simulation | balance:%d\n\n" % self.pool_balance()
         for comb in self.all_honeycombs:
             str_val += '%s\n' % comb
         return str_val
@@ -71,27 +75,9 @@ class Honeycomb:
         self.bees = []
         self.hive.all_honeycombs.append(self)
 
-    def join_bee__(self, bee, fee):
+    def join_bee__(self, bee):
         # logging.debug("bee:%s", bee)
         self.bees.append(bee)
-        self.hive.renew(fee)
-
-    def charge(self, premium):
-        sum = reduce((lambda x, y: x + y), [b.balance for b in self.bees])
-        if sum > premium:
-            # charge everyone in same comb
-            for bee in self.bees:
-                ratio = bee.balance / sum
-                bee.balance -= premium * ratio
-        else:
-            delta = premium - sum
-            for bee in self.bees:
-                bee.balance = 0
-
-            remaining = self.hive.balance - delta
-            self.hive.balance = remaining
-            if remaining < 0:
-                raise BankruptException('OMG, I bankrupted!')
 
     def balance(self):
         return reduce((lambda x, y: x + y), [b.balance for b in self.bees])
@@ -105,7 +91,7 @@ class Honeycomb:
 
 class Bee:
 
-    def __init__(self, bee_id, premium, comb, pool_ratio=defalut_small_pool_ratio):
+    def __init__(self, bee_id, premium, comb, pool_ratio=conf.small_pool_ratio):
         self.id = bee_id
         self.honeycomb = comb
         self.premium = premium
@@ -114,7 +100,8 @@ class Bee:
 
         small_pool = int(math.floor(self.premium * self.small_pool_ratio))
         self.balance += small_pool
-        self.honeycomb.join_bee__(self, premium - small_pool)
+        self.pool_balance = premium - small_pool
+        self.honeycomb.join_bee__(self)
 
     def charge(self, fee):
         remaining = self.balance - fee
@@ -123,21 +110,31 @@ class Bee:
             return 0
         else:
             self.balance = 0
-            self.honeycomb.charge(-1 * remaining)
-            return remaining
+            remain_tmp = -1 * remaining
+
+            if self.pool_balance >= remain_tmp:
+                self.pool_balance -= remain_tmp
+                return 0
+
+            self.pool_balance = 0
+            remain_fee = remain_tmp - self.pool_balance
+
+            self.honeycomb.hive.charge(self, remain_fee)
+            return remain_fee
 
     def renew(self):
         small_pool = self.premium * self.small_pool_ratio
         self.balance += small_pool
-        self.honeycomb.hive.renew(self.premium - small_pool)
+        self.pool_balance = self.premium - small_pool
 
     def __str__(self):
-        return '[Bee:%d | comb_id:%d | balance:%d]' % (self.id, self.honeycomb.id, self.balance)
+        return '[Bee:%d | comb_id:%d | balance:%d | pool_balance:%d]' % \
+               (self.id, self.honeycomb.id, self.balance, self.pool_balance)
 
 
-class Stats:
+class Simulation:
 
-    def __init__(self, comb_size, bee_size, days, ratio, output_fig='beehive-simulation.png'):
+    def __init__(self, comb_size, bee_size, days, ratio, calc_max_premium, output_fig='beehive-simulation.png'):
         self.honeycomb_size = comb_size
         self.bee_size = bee_size
         self.days = days
@@ -146,41 +143,42 @@ class Stats:
 
         self.the_comb_id = 0
         self.the_bee_id = 0
-        self.the_hive = Beehive()
+        self.the_hive = Beehive(0, conf.global_reserve_fund, calc_max_premium)
+        self.calc_max_premium = calc_max_premium
 
     @staticmethod
     def generate_premium(size):
-        return np.random.normal(premium_mu, premium_sigma, size)
+        return np.random.normal(conf.premium_mu, conf.premium_sigma, size)
 
     @staticmethod
     def generate_charge(size):
-        return np.random.normal(charge_mu, charge_sigma, size)
+        return np.random.normal(conf.charge_mu, conf.charge_sigma, size)
 
     @staticmethod
     def generate_claim_event(days):
-        return np.random.poisson(claim_freq_lambda, days)
+        return np.random.poisson(conf.claim_freq_lambda, days)
 
     def simulate(self):
-        premiums = Stats.generate_premium(self.honeycomb_size * self.bee_size)
-        for i in xrange(honeycomb_size):
+        premiums = Simulation.generate_premium(self.honeycomb_size * self.bee_size)
+        for i in xrange(self.honeycomb_size):
             self.the_comb_id += 1
             comb = Honeycomb(self.the_comb_id, self.the_hive)
             # logging.debug("honeycomb:%d", the_comb_id)
 
-            for j in xrange(bee_size):
+            for j in xrange(self.bee_size):
                 self.the_bee_id += 1
-                premium = premiums[bee_size * i + j]
-                bee = Bee(self.the_bee_id, premium, comb, pool_ratio)
+                premium = premiums[self.bee_size * i + j]
+                bee = Bee(self.the_bee_id, premium, comb, self.pool_ratio)
                 # logging.debug("bee:%d", bee)
 
         # logging.debug("Beehive:%s", the_hive)
 
         day_cntr = 0
         evt_sum = 0
-        for evt_num in Stats.generate_claim_event(days):
+        for evt_num in Simulation.generate_claim_event(self.days):
             day_cntr += 1
             try:
-                charges = Stats.generate_charge(evt_num)
+                charges = Simulation.generate_charge(evt_num)
                 for i in xrange(evt_num):
                     evt_sum += 1
                     bee = random.choice(self.the_hive.bees())
@@ -196,7 +194,15 @@ class Stats:
         # logging.info("Remaining:%d in Beehive", the_hive.balance)
 
 
-def output_config(honeycomb_size, bee_size, sim_count, days, ratio, output_fig):
+def calc_max_premium_constant(pool_balance):
+    return conf.max_premium_constant
+
+
+def calc_max_premium_ratio(pool_balance):
+    return pool_balance * conf.max_premium_ratio
+
+
+def output_config(honeycomb_size, bee_size, sim_count, days, ratio):
     line_str = "====================================================================="
     config_str = "%s\n" \
                  "  蜂巢小组总数:\t\t%d\n" \
@@ -204,23 +210,23 @@ def output_config(honeycomb_size, bee_size, sim_count, days, ratio, output_fig):
                  "  模拟总次数:\t\t%d\n" \
                  "  模拟总天数:\t\t%d\n" \
                  "  小池留存比例:\t\t%s\n" \
-                 "  输出图像文件:\t\t%s\n" \
                  "%s\n" \
                  "  赔付频率泊松过程:\tlambda:%d\n" \
                  "  投保金额高斯分布:\tmu:%d, sigma:%d\n" \
                  "  赔付金额高斯分布:\tmu:%d, sigma:%d\n" \
                  "%s\n" \
                  % (line_str,
-                    honeycomb_size, bee_size, sim_count, days, ratio, output_fig,
+                    honeycomb_size, bee_size, sim_count, days, ratio,
                     line_str,
-                    claim_freq_lambda,
-                    premium_mu, premium_sigma,
-                    charge_mu, charge_sigma,
+                    conf.claim_freq_lambda,
+                    conf.premium_mu, conf.premium_sigma,
+                    conf.charge_mu, conf.charge_sigma,
                     line_str)
     logging.info(config_str)
 
 
-def output_figure(beehive, fig=default_fig_name):
+@staticmethod
+def output_figure(beehive, fig=conf.default_fig_name):
     import matplotlib.pyplot as plt
     from pylab import mpl
 
@@ -235,34 +241,25 @@ def output_figure(beehive, fig=default_fig_name):
     ax.set_ylabel(u'小组数量')
     ax.set_title(u'各小组余额分布')
     plt.savefig(fig)
-    # plt.show()
+    plt.show()
 
 
 if __name__ == "__main__":
     logging.basicConfig(format='%(message)s', level=logging.INFO)
-    logging.info("Simulating Beehive...\n")
+    logging.info("蜂巢投保模拟程序: Simulating Beehive...\n")
 
-    parser = argparse.ArgumentParser(description='蜂巢投保模拟程序')
-    parser.add_argument('--sim_count', '-C', type=int, default=1, help='模拟总次数')
-    parser.add_argument('--comb_num', '-N', type=int, default=default_honeycomb_size_in_hive, help='模拟的蜂巢小组总数')
-    parser.add_argument('--bee_num', '-n', type=int, default=default_bee_size_in_honeycomb, help='模拟的每个小组总人数')
-    parser.add_argument('--days', '-d', type=int, default=N_Days, help='模拟总天数')
-    parser.add_argument('--pool_ratio', '-r', type=float, default=defalut_small_pool_ratio, help='小池留存比例')
-    parser.add_argument('--output_fig', '-o', type=str, default=default_fig_name, help='分析图表文件名')
+    output_config(conf.honeycomb_size_in_hive,
+                  conf.bee_size_in_honeycomb,
+                  conf.simulation_count,
+                  conf.N_Days,
+                  conf.small_pool_ratio)
 
-    args = parser.parse_args()
+    simulation = Simulation(conf.honeycomb_size_in_hive,
+                            conf.bee_size_in_honeycomb,
+                            conf.N_Days,
+                            conf.small_pool_ratio,
+                            calc_max_premium_ratio)
 
-    sim_count = args.sim_count
-    honeycomb_size = args.comb_num
-    bee_size = args.bee_num
-    days = args.days
-    pool_ratio = args.pool_ratio
-    output_fig = args.output_fig
-
-    output_config(honeycomb_size, bee_size, sim_count, days, pool_ratio, output_fig)
-    stats = Stats(honeycomb_size, bee_size, days, pool_ratio, output_fig)
-
-    for sim_time in xrange(sim_count):
-        stats.simulate()
-        output_figure(stats.the_hive)
+    for sim_time in xrange(conf.simulation_count):
+        simulation.simulate()
 
---
+++ conf.py
@@ -0,0 +1,37 @@
+# encoding: utf8
+
+# 分析图表文件名
+default_fig_name = 'beehive-simulation.png'
+
+# 小池留存比例
+small_pool_ratio = 0.25
+
+# 模拟总次数
+simulation_count = 1
+
+# 模拟总天数
+N_Days = 365
+
+claim_freq_lambda = 10
+
+# 模拟的蜂巢小组总数
+honeycomb_size_in_hive = 300
+
+# 模拟的每个小组总人数
+bee_size_in_honeycomb = 5
+
+# 投保分布参数
+premium_mu = 4000
+premium_sigma = 2000
+
+# 索赔金额分布参数
+charge_mu = 500
+charge_sigma = 200
+
+# 最高赔付金额
+max_premium_constant = 100
+max_premium_ratio = 0.05
+
+# 准备金总额
+global_reserve_fund = 3000000
+
